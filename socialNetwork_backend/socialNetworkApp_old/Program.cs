4c4cfe4844b3a82e7c0011528c35983ede2daba3
DaniinXorchenabo <rkbcu@mail.ru>
2022-04-02 18:17:23 +0300

    онечные точки определяются полностью через атрибуты, `swagger` отображает конечные точки, определённые через атрибуты.

--- socialNetwork_backend/socialNetworkApp_old/Program.cs
+++ socialNetwork_backend/socialNetworkApp_old/Program.cs
@@ -1,9 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+// builder.Services
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
+
 app.UseSwagger();
 app.UseSwaggerUI();
-app.MapGet("/", () => "Hello World!");
+
+
+app.MapControllers();
 
 await app.RunAsync();
