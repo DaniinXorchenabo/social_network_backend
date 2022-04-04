c5a31ea8d74f255894eb50507817cf368bb81ad5
DaniinXorchenabo <rkbcu@mail.ru>
2022-04-04 16:54:20 +0300

    added a simple useless middleware.

---
+++ socialNetwork_backend/socialNetworkApp_old/src/api/middlewares/BaseAnswerMiddleware.cs
@@ -0,0 +1,17 @@
+﻿namespace socialNetworkApp.api.middlewares;
+
+public class BaseAnswerMiddleware
+{
+    private readonly RequestDelegate next;
+    
+    public BaseAnswerMiddleware(RequestDelegate next)
+    {
+        this.next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        await next.Invoke(context);
+        
+    }
+}
