68ff1feefef5893e68e140f396e391a859e8a325
DaniinXorchenabo <rkbcu@mail.ru>
2022-04-06 19:48:08 +0300

    added environ variables loader from `.env` file.

--- socialNetwork_backend/socialNetworkApp/Program.cs
+++ socialNetwork_backend/socialNetworkApp/Program.cs
@@ -1,5 +1,6 @@
 using System.Net.Mime;
 using System.Text.Json.Serialization;
+using dotenv.net;
 // using System.Web.Http.Controllers;
 using Microsoft.OpenApi.Models;
 using socialNetworkApp.api.middlewares;
@@ -12,6 +13,28 @@ class Program
 {
     static async Task Main(string[] args)
     {
+        
+        const string envFileName = ".env";
+        
+        var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+        while ((baseDirectory = Path.GetDirectoryName(baseDirectory))?.EndsWith("socialNetwork_backend") == false)
+        {
+            Console.WriteLine(baseDirectory);
+        }
+        
+        Console.WriteLine($"{baseDirectory}/{envFileName}");
+            
+        DotEnv.Load(options: new DotEnvOptions(envFilePaths: new[] { Path.Combine(baseDirectory, envFileName) }));
+        
+        var dbHost = System.Environment.GetEnvironmentVariable("DB_HOST") ?? "localhost";
+        var dbPort = System.Environment.GetEnvironmentVariable("DB_PORT") ?? "5432";
+        var dbUsername = System.Environment.GetEnvironmentVariable("DB_SUPERUSER_NAME") ?? "";
+        var dbPassword = System.Environment.GetEnvironmentVariable("DB_SUPERUSER_PASSWORD") ?? "";
+        var dbName = System.Environment.GetEnvironmentVariable("DB_DATABASE_NAME") ?? "some";
+        
+        Console.WriteLine($"{dbHost}, {dbPort}, {dbUsername}, {dbPassword}, {dbName}");
+
         var builder = WebApplication.CreateBuilder(args);
 // HttpRequestContext config = new HttpRequestContext();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
