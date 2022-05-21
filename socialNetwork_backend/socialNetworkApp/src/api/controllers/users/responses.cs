b354031854e91c57a922ebeeed11339c5a0c4e85
DaniinXorchenabo <rkbcu@mail.ru>
2022-05-21 22:53:53 +0300

    added responses for user controller.

---
+++ socialNetwork_backend/socialNetworkApp/src/api/controllers/users/responses.cs
@@ -0,0 +1,26 @@
+﻿using socialNetworkApp.api.dtos;
+using socialNetworkApp.api.enums;
+using socialNetworkApp.api.responses;
+using socialNetworkApp.api.responses.utils;
+
+
+namespace socialNetworkApp.api.controllers.users;
+
+
+[AddAnswerType(AnswerType.UserAnswer)]
+public  class UserAnswer<TValidate> : BaseResponse<GetUser, TValidate> where TValidate: AbstractDto
+{
+    public UserAnswer(params dynamic[] errAndAns) : base(errAndAns)
+    {
+        
+    }
+}
+
+
+[AddAnswerType(AnswerType.UserAnswer)]
+public class UserAnswer : UserAnswer<AbstractDto>
+{
+    public UserAnswer(params dynamic[] errAndAns) : base(errAndAns)
+    {
+    }
+}
