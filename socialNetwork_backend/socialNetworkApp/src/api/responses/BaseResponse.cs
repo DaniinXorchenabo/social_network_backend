03a97cbf5d85d565a04769307427f7986cd24e50
DaniinXorchenabo <rkbcu@mail.ru>
2022-05-21 22:49:55 +0300

    Добавил один большой конструктор через `params dynamic[]` для обработки всего, что пришло в `BaseResponse`, добавил так же интерфейсы `IBaseResponse` для ковариантности дженериков.

--- socialNetwork_backend/socialNetworkApp/src/api/responses/BaseResponse.cs
+++ socialNetwork_backend/socialNetworkApp/src/api/responses/BaseResponse.cs
@@ -1,47 +1,299 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections;
+using System.ComponentModel;
+using System.Dynamic;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
+using socialNetworkApp.api.dtos;
 using socialNetworkApp.api.enums;
 using socialNetworkApp.api.responses.utils;
+using socialNetworkApp.db;
 
 namespace socialNetworkApp.api.responses;
 
+public abstract class BaseResponse
+{
+}
 
-public class BaseResponse<TAnswer> where TAnswer : EmptyAnswer
+public interface IBaseResponse<out TAnswer, out TValidate, out TError>
+{
+}
+
+public interface IBaseResponse<out TAnswer, out TValidate> : IBaseResponse<TAnswer, TValidate, EmptyError>
+{
+}
+
+public interface IBaseResponse<out TAnswer> : IBaseResponse<TAnswer, AbstractDto, EmptyError>
+{
+}
+
+public class BaseResponse<TAnswer, TValidate, TError> : BaseResponse, IBaseResponse<TAnswer, TValidate, TError>
+    where TAnswer : EmptyAnswer
+    where TValidate : AbstractDto
+    where TError : EmptyError
 {
     public List<BaseAnswerRes<TAnswer>>? Answers { get; set; }
-    public List<EmptyError>? Errors { get; set; }
+    public List<TError>? Errors { get; set; }
 
-    public BaseResponse(List<BaseAnswerRes<TAnswer>>? ans = null, List<EmptyError>? err = null)
-    {
-        this.Answers = ans;
-        this.Errors = err;
+    // public BaseResponse(List<BaseAnswerRes<TAnswer>>? ans = null, List<TError>? err = null)
+    // {
+    //     this.Answers = ans;
+    //     this.Errors = err;
+    // }
+    //
+    // public BaseResponse(List<TAnswer>? ans = null, List<TError>? err = null)
+    // {
+    //     AnswerType type = GetAnswerType();
+    //     this.Answers = ans?.Select(x => new BaseAnswerRes<TAnswer>(type, x)).ToList();
+    //     this.Errors = err;
+    // }
+    //
+    // public BaseResponse(List<TError>? err = null, params TAnswer[] ans)
+    // {
+    //     AnswerType type = GetAnswerType();
+    //     this.Answers = ans.Select(x => new BaseAnswerRes<TAnswer>(type, x)).ToList();
+    //     this.Errors = err;
+    // }
+    //
+    // public BaseResponse(params BaseResponsePart[] errAndAns)
+    // {
+    //     AnswerType type = GetAnswerType();
+    //     this.Answers = errAndAns
+    //         .Where(x => x is TAnswer)
+    //         .Select(x => new BaseAnswerRes<TAnswer>(type, (x as TAnswer)!))
+    //         .ToList();
+    //     this.Errors = errAndAns
+    //         .Where(x => x is TError)
+    //         .Select(x => (x as TError)!)
+    //         .ToList();
+    // }
+    //
+    // public BaseResponse(List<BaseResponsePart> errAndAns)
+    // {
+    //     AnswerType type = GetAnswerType();
+    //     this.Answers = errAndAns
+    //         .Where(x => x is TAnswer)
+    //         .Select(x => new BaseAnswerRes<TAnswer>(type, (x as TAnswer)!))
+    //         .ToList();
+    //     this.Errors = errAndAns
+    //         .Where(x => x is TError)
+    //         .Select(x => (x as TError)!)
+    //         .ToList();
+    // }
 
-    }
-    public BaseResponse(List<TAnswer>? ans = null, List<EmptyError>? err = null)
+    // public static TContainsType? CheckGenericTypeFunc<TContainsType>(dynamic val)
+    // {
+    //     if (val is TContainsType resultValue)
+    //     {
+    //         return resultValue;
+    //     }
+    //     if (val.GetType().IsGenericType)
+    //     {
+    //         var genericContainType = typeof(TContainsType).GetGenericTypeDefinition();
+    //         if (val.GetType().GetGenericTypeDefinition() == genericContainType)
+    //         {
+    //             var targetValueType = typeof(TContainsType).GenericTypeArguments[0];
+    //
+    //             if ((val.GetType() as Type).GenericTypeArguments[0].IsSubclassOf(targetValueType))
+    //             {
+    //                 return val;
+    //             }
+    //         }
+    //
+    //     }
+    //
+    //     return null;
+    // }
+
+    public BaseResponse(params dynamic[] values)
     {
-        AnswerType type = GetAnswerType();
-        this.Answers = ans?.Select(x => new BaseAnswerRes<TAnswer>(type, x)).ToList();
-        this.Errors = err;
+        this.Errors = new List<TError>();
+        this.Answers = new List<BaseAnswerRes<TAnswer>>();
+
+        oneParamProcessing(values);
+        
+        // foreach (var value in values)
+        // {
+        //
+        // var testFunc = (Type checkedVal, dynamic val, Type genericContainType) => 
+
+            // if (value is List<BaseResponsePart> valueAsList)
+            // {
+            //     AnswerType type = GetAnswerType();
+            //     this.Answers.AddRange(valueAsList
+            //         .Where(x => x is TAnswer)
+            //         .Select(x => new BaseAnswerRes<TAnswer>(type, (x as TAnswer)!))
+            //         .ToList());
+            //     this.Errors.AddRange(valueAsList
+            //         .Where(x => x is TError)
+            //         .Select(x => (x as TError)!)
+            //         .ToList());
+            // }
+            // else if (value is BaseResponsePart[] valueAsArray)
+            // {
+            //     AnswerType type = GetAnswerType();
+            //     this.Answers.AddRange(valueAsArray
+            //         .Where(x => x is TAnswer)
+            //         .Select(x => new BaseAnswerRes<TAnswer>(type, (x as TAnswer)!))
+            //         .ToList());
+            //     this.Errors.AddRange(valueAsArray
+            //         .Where(x => x is TError)
+            //         .Select(x => (x as TError)!)
+            //         .ToList());
+            // }
+            // else if (value is BaseResponsePart valueAsBaseResponsePart)
+            // {
+            //     if (valueAsBaseResponsePart is TAnswer valueAsTAns)
+            //     {
+            //         this.Answers.Add(new BaseAnswerRes<TAnswer>(GetAnswerType(), valueAsTAns));
+            //     }
+            //     else if (valueAsBaseResponsePart is TError valueAsTErr)
+            //     {
+            //         this.Errors.Add(valueAsTErr);
+            //     }
+            // }
+            // else if (value is List<TError> valueAsTErrorList)
+            // {
+            //     this.Errors.AddRange(valueAsTErrorList);
+            // }
+            // else if (value is TError[] valueAsTErrorArray)
+            // {
+            //     this.Errors.AddRange(valueAsTErrorArray.ToList());
+            // }
+            // else if (value is TError valueAsTError)
+            // {
+            //     this.Errors.Add(valueAsTError);
+            // }
+            // else if (value is List<TAnswer> valueAsTAnswerList)
+            // {
+            //     AnswerType type = GetAnswerType();
+            //     this.Answers.AddRange(valueAsTAnswerList
+            //         .Where(x => x is TAnswer)
+            //         .Select(x => new BaseAnswerRes<TAnswer>(type, (x as TAnswer)!))
+            //         .ToList());
+            // }
+            // else if (value is TAnswer[] valueAsTAnswerArray)
+            // {
+            //     AnswerType type = GetAnswerType();
+            //     this.Answers.AddRange(valueAsTAnswerArray
+            //         .Where(x => x is TAnswer)
+            //         .Select(x => new BaseAnswerRes<TAnswer>(type, (x as TAnswer)!))
+            //         .ToList());
+            // }
+            // else if (value is TAnswer valueAsTAnswer)
+            // {
+            //     this.Answers.Add(new BaseAnswerRes<TAnswer>(GetAnswerType(), valueAsTAnswer));
+            // }
+            // else if (value is List<BaseAnswerRes<TAnswer>> valueAsBaseRTAnswerList)
+            // {
+            //     this.Answers.AddRange(valueAsBaseRTAnswerList);
+            // }
+            // else if (value is BaseAnswerRes<TAnswer>[] valueAsBaseRTAnswerArray)
+            // {
+            //     this.Answers.AddRange(valueAsBaseRTAnswerArray.ToList());
+            // }
+            // else if (value is BaseAnswerRes<TAnswer> valueAsBaseRTAnswer)
+            // {
+            //     this.Answers.Add(valueAsBaseRTAnswer);
+            // }
+            // else if (value is List<AbstractEntity> valueAsAbstractEntityList)
+            // {
+            //     Type dtoType = this.GetType()
+            //         .GetProperty("Answers")
+            //         .PropertyType
+            //         .GenericTypeArguments[0]
+            //         .GenericTypeArguments[0];
+            //     this.Answers.AddRange(
+            //         valueAsAbstractEntityList
+            //             .Select(x => (Activator.CreateInstance(dtoType, new object[] {x}) as TAnswer))
+            //             .Select(x => new BaseAnswerRes<TAnswer>(GetAnswerType(), x)).ToList());
+            // }
+            // else if (value is AbstractEntity[] valueAsAbstractEntityArray)
+            // {
+            //     Type dtoType = this.GetType()
+            //         .GetProperty("Answers")
+            //         .PropertyType
+            //         .GenericTypeArguments[0]
+            //         .GenericTypeArguments[0];
+            //     this.Answers.AddRange(
+            //         valueAsAbstractEntityArray
+            //             .Select(x => (Activator.CreateInstance(dtoType, new object[] {x}) as TAnswer))
+            //             .Select(x => new BaseAnswerRes<TAnswer>(GetAnswerType(), x)).ToList());
+            // }
+            // else if (value is AbstractEntity valueAsAbstractEntity)
+            // {
+            //     Type dtoType = this.GetType()
+            //         .GetProperty("Answers")
+            //         .PropertyType
+            //         .GenericTypeArguments[0]
+            //         .GenericTypeArguments[0];
+            //     TAnswer answerFromDbModel =
+            //         (Activator.CreateInstance(dtoType, new object[] {valueAsAbstractEntity}) as TAnswer);
+            //     this.Answers.Add(new BaseAnswerRes<TAnswer>(GetAnswerType(), answerFromDbModel));
+            // }
+        // }
+
+        if (this.Errors.Count == 0)
+        {
+            this.Errors = null;
+        }
+
+        if (this.Answers.Count == 0)
+        {
+            this.Answers = null;
+        }
     }
-    public BaseResponse(List<EmptyError>? err = null, params TAnswer[] ans)
+
+    public void oneParamProcessing(dynamic value)
     {
-        AnswerType type = GetAnswerType();
-        this.Answers = ans.Select(x => new BaseAnswerRes<TAnswer>(type, x)).ToList();
-        this.Errors = err;
+        if (value is IEnumerable valueEnumerator)
+        {
+            foreach (var o in valueEnumerator)
+            {
+                oneParamProcessing(o);
+            }
+        }
+        else
+        {
+            if (value is BaseResponsePart valueAsBaseResponsePart)
+            {
+                if (valueAsBaseResponsePart is TAnswer valueAsTAns)
+                {
+                    this.Answers.Add(new BaseAnswerRes<TAnswer>(GetAnswerType(), valueAsTAns));
+                }
+                else if (valueAsBaseResponsePart is TError valueAsTErr)
+                {
+                    this.Errors.Add(valueAsTErr);
+                }
+            }
+            else if (value is TError[] valueAsTErrorArray)
+            {
+                this.Errors.AddRange(valueAsTErrorArray.ToList());
+            }
+            else if (value is TAnswer valueAsTAnswer)
+            {
+                this.Answers.Add(new BaseAnswerRes<TAnswer>(GetAnswerType(), valueAsTAnswer));
+            }
+            else if (value is BaseAnswerRes<TAnswer> valueAsBaseRTAnswer)
+            {
+                this.Answers.Add(valueAsBaseRTAnswer);
+            }
+            else if (value is AbstractEntity valueAsAbstractEntity)
+            {
+                Type dtoType = this.GetType()
+                    .GetProperty("Answers")
+                    .PropertyType
+                    .GenericTypeArguments[0]
+                    .GenericTypeArguments[0];
+                TAnswer answerFromDbModel = Activator.CreateInstance(dtoType,  valueAsAbstractEntity) as TAnswer;
+                this.Answers.Add(new BaseAnswerRes<TAnswer>(GetAnswerType(), answerFromDbModel));
+            }
+        }
     }
-    public BaseResponse(params BaseResponsePart[] errAndAns)
+
+    public BaseResponse()
     {
-        AnswerType type = GetAnswerType();
-        this.Answers = errAndAns
-            .Where(x => x is TAnswer)
-            .Select(x => new BaseAnswerRes<TAnswer>(type, (x as TAnswer)!))
-            .ToList();
-        this.Errors = errAndAns
-            .Where(x => x is EmptyError)
-            .Select(x => (x as EmptyError)!)
-            .ToList();
     }
-    
+
 
     protected AnswerType GetAnswerType()
     {
@@ -52,8 +304,8 @@ public class BaseResponse<TAnswer> where TAnswer : EmptyAnswer
         {
             return attr!.Type;
         }
+
         throw new Exception($"Аттрибут не задан у класса {typeof(TAnswer).Name}");
-        
     }
 
     public Task ExecuteResultAsync(ActionContext context)
@@ -61,10 +313,66 @@ public class BaseResponse<TAnswer> where TAnswer : EmptyAnswer
         // OkObjectResult
         throw new NotImplementedException();
     }
-
-    public int? StatusCode { get; }
 }
 
+public class BaseResponse<TAnswer, TValidate>
+    : BaseResponse<TAnswer, TValidate, EmptyError>,
+        IBaseResponse<TAnswer, TValidate>
+    where TAnswer : EmptyAnswer
+    where TValidate : AbstractDto
+{
+    // public BaseResponse(List<BaseAnswerRes<TAnswer>>? ans = null, List<EmptyError>? err = null) : base(ans, err)
+    // {
+    // }
+    //
+    // public BaseResponse(List<TAnswer>? ans = null, List<EmptyError>? err = null) : base(ans, err)
+    // {
+    // }
+    //
+    // public BaseResponse(List<EmptyError>? err = null, params TAnswer[] ans) : base(err, ans)
+    // {
+    // }
+    //
+    // public BaseResponse(params BaseResponsePart[] errAndAns) : base(errAndAns)
+    // {
+    // }
+    public BaseResponse(params dynamic[] values) : base(values)
+    {
+    }
+
+    protected BaseResponse()
+    {
+    }
+}
+
+public class BaseResponse<TAnswer> : 
+    BaseResponse<TAnswer, AbstractDto, EmptyError>,
+    IBaseResponse<TAnswer>
+    where TAnswer : EmptyAnswer
+{
+    // public BaseResponse(List<BaseAnswerRes<TAnswer>>? ans = null, List<EmptyError>? err = null) : base(ans, err)
+    // {
+    // }
+    //
+    // public BaseResponse(List<TAnswer>? ans = null, List<EmptyError>? err = null) : base(ans, err)
+    // {
+    // }
+    //
+    // public BaseResponse(List<EmptyError>? err = null, params TAnswer[] ans) : base(err, ans)
+    // {
+    // }
+    //
+    // public BaseResponse(params BaseResponsePart[] errAndAns) : base(errAndAns)
+    // {
+    // }
+    public BaseResponse(params dynamic[] values) : base(values)
+    {
+    }
+
+    protected BaseResponse()
+    {
+    }
+}
 
 public class Resp : ObjectResult, IActionResult, IStatusCodeActionResult
 {
@@ -74,6 +382,6 @@ public class Resp : ObjectResult, IActionResult, IStatusCodeActionResult
     }
 }
 
-public record class BaseResponsePart
+public class BaseResponsePart
 {
 }
