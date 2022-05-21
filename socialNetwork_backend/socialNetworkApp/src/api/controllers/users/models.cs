1ea6f38e841485d4d296a462fbd9997735b0571c
DaniinXorchenabo <rkbcu@mail.ru>
2022-05-21 23:00:54 +0300

    чуть-чуть поменял структуру пользовательской модели, добавил атрибутов для большей конкретики

--- socialNetwork_backend/socialNetworkApp/src/api/controllers/users/models.cs
+++ socialNetwork_backend/socialNetworkApp/src/api/controllers/users/models.cs
@@ -12,27 +12,31 @@ namespace socialNetworkApp.api.controllers.users;
 public class UserDb : AbstractEntity
 {
     // [Comment("The URL of the blog")]
-    [Key][Required] [Column("id")] public virtual Guid Id { get; set; }
+    [Key] [Required] [Column("id")] public virtual Guid Id { get; set; }
 
-    [Unicode][Required][Column("username")] public virtual string Username { get; set; }
+    [Unicode]
+    [Required]
+    [Column("username")]
+    public virtual string Username { get; set; }
 
-    [Unicode][Required][Column("email")] public virtual string Email { get; set; } 
-    [Required][Column("hashed_password")] public virtual string HashedPassword { get; set; }
-    [Required][Column("name")] public virtual string Name { get; set; }
-    [Required][Column("surname")] public virtual string Surname { get; set; }
+    [Unicode] [Required] [Column("email")] public virtual string Email { get; set; }
+    [Required] [Column("hashed_password")] public virtual string HashedPassword { get; set; }
+    [Required] [Column("name")] public virtual string Name { get; set; }
+    [Required] [Column("surname")] public virtual string Surname { get; set; }
+    //TODO: 
+    // [Required] [Column("is_deleted")] public virtual bool IsDeleted { get; set; } = false;
 
-    [Required] [Column("mods" )]// [Column("mods", TypeName = "integer[]")]
+    [Required]
+    [Column("mods")] // [Column("mods", TypeName = "integer[]")]
     public virtual List<AllModsEnum> Mods { get; set; } = new List<AllModsEnum>();
 
     // public virtual List<ModifiersOfAccessDb> Mods { get; set; } = new List<ModifiersOfAccessDb>();
 
     public UserDb(object obj) : base(obj)
     {
-        
     }
 
     public UserDb() : base()
     {
-        
     }
 }
