74b1b0eac6a52ca7e67a34ac7c8871450585c593
DaniinXorchenabo <rkbcu@mail.ru>
2022-05-21 22:38:54 +0300

    безуспешные попытки отобразить `Dictionary<Enum, >` в `Swagger`.

---
+++ socialNetwork_backend/socialNetworkApp/src/api/attributes/DictEnumSwaggerAttribute.cs
@@ -0,0 +1,11 @@
+﻿using SuccincT.PatternMatchers;
+
+namespace socialNetworkApp.api.attributes;
+
+public class DictEnumSwaggerAttribute : Attribute 
+{
+    // public DictEnumSwaggerAttribute<T>()
+    // {
+    //
+    // }
+}
