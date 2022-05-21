6204de541f31e73b10875d404879cbf95c7e88c7
DaniinXorchenabo <rkbcu@mail.ru>
2022-05-21 22:42:47 +0300

    Added a structure for validation error displaying.

---
+++ socialNetwork_backend/socialNetworkApp/src/api/responses/error/ValidateError.cs
@@ -0,0 +1,31 @@
+﻿using socialNetworkApp.api.attributes;
+
+namespace socialNetworkApp.api.responses.error;
+
+public class ValidateError<TEnum>: BaseErrorRes where TEnum: Enum
+{
+    [DictEnumSwagger]
+    public Dictionary<string, ValidateOneField> Errors { get; set; }
+    public TEnum testEnum { get; set; }
+    
+}
+
+// public  class ValidateError//: BaseErrorRes
+// {
+//     // public Dictionary<string, ValidateOneField> Errors { get; set; }
+//     
+// }
+
+
+public  class ValidateOneField
+{
+    public string FieldName { get; set; }
+    public List<OneFieldErrorValidate> Errors { get; set; }
+    public string ValidationState { get; set; }
+}
+
+   public class OneFieldErrorValidate
+{
+    public string? Exception { get; set; }
+    public string? ErrorMessage { get; set; }
+}
