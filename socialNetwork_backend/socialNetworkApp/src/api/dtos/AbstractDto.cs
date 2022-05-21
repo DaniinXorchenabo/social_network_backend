a271e1109f66893a653f43c5c3dd90e7e34b7104
DaniinXorchenabo <rkbcu@mail.ru>
2022-05-21 22:50:35 +0300

    Теперь `dto`хи могут создаваться из моделей базы данных.

--- socialNetwork_backend/socialNetworkApp/src/api/dtos/AbstractDto.cs
+++ socialNetwork_backend/socialNetworkApp/src/api/dtos/AbstractDto.cs
@@ -1,10 +1,40 @@
 ﻿using System.Reflection;
+using socialNetworkApp.api.responses;
 using socialNetworkApp.db;
 
 namespace socialNetworkApp.api.dtos;
 
-public class AbstractDto
+public class AbstractDto: EmptyAnswer
 {
+    
+    public AbstractDto(object obj)
+    {
+        var myStaticData = DtoStaticData.AllClasses[this.GetType()];
+        var ObjFields = EntityStaticData.AllClasses[obj.GetType()].PropertiesAsString;
+        var moveFields = new HashSet<string>(ObjFields);
+        moveFields.IntersectWith(myStaticData.PropertiesAsString);
+        
+        Console.WriteLine($"-------------- {this.GetType().Name}");
+        Console.WriteLine(string.Join(", ", ObjFields));
+        Console.WriteLine(string.Join(", ", myStaticData.PropertiesAsString));
+        Console.WriteLine(string.Join(", ", moveFields));
+        Console.WriteLine("--------------");
+        
+        
+        foreach (var moveField in moveFields)
+        {
+            this.GetType().GetProperty(moveField).SetValue(
+                this,
+                Convert.ChangeType(
+                    obj.GetType().GetProperty(moveField).GetValue(obj),
+                    this.GetType().GetProperty(moveField).PropertyType)
+            );
+        }
+    }
+
+    public AbstractDto()
+    {
+    }
 
     public override string ToString()
     {
